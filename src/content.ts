62659ab86081b6a63cb2f6935f54498e73e916e4
Thiago Lopes Trugillo da Silveira <823457+tltsilveira@users.noreply.github.com>
2026-09-15 19:19:12 -0300

    Update content.ts

--- src/content.ts
+++ src/content.ts
@@ -678,6 +678,13 @@ export const NEWS: NewsItem[] = [
   {
     date: "Sep 2026",
     tag: "Publication",
+    text: "VisCo just had a paper accepted for presentation at VCIP!",
+    img: "/news/f59c8b32-6a14-41d3-9ee8-124b5d63f9db.png",
+    href: "publications",
+  },
+  {
+    date: "Sep 2026",
+    tag: "Publication",
     text: "VisCo just had a paper accepted for presentation at SEPOC!",
     img: "/news/f59c8b32-6a14-41d3-9ee8-124b5d63f9da.png",
     href: "publications",
